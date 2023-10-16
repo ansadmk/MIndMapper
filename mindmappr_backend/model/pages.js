2741e44a281ab1bdfbe8c2bf635ac34bf51f385e
ansadmk <ansadmk@gmail.com>
2023-10-16 12:59:19 +0530

    schema

---
+++ mindmappr_backend/model/pages.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose')
+const pageSchema=mongoose.Schema({
+    title:String,
+    owner:mongoose.Schema.ObjectId,
+    content:String,
+    createdAt:Date,
+    updatedAt:Date,
+    settings:Object
+
+})
+
+module.exports=mongoose.model("pages",userSchema)
--- mindmappr_backend/model/user.js
+++ mindmappr_backend/model/user.js
@@ -3,7 +3,11 @@ const userSchema=mongoose.Schema({
     username:String,
     password:String,
     email:String,
-    image:String
+    image:String,
+    fullName:String,
+    createdAt:Date,
+    accountinfo:Object
+    
 
 })
 
