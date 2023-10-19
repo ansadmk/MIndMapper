21424201b5189cb54499358b34effddffc489d5e
ansadmk <ansadmk@gmail.com>
2023-10-19 11:27:26 +0530

    create page

--- mindmappr_js_frontend/src/app/Users/components/welcomepage.jsx
+++ mindmappr_js_frontend/src/app/Users/components/welcomepage.jsx
@@ -1,12 +1,10 @@
 'use client'
-import { signIn,signOut,useSession } from "next-auth/react"
-import { Button } from "react-bootstrap"
 import NavBar from "./NavBar"
 
 
 
 export default function Welcome() {
-  const {data}=useSession()
+ 
  
   return (
     <div className="position-relative ">
