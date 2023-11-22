4cb3d2646df23c18770a1e333fdfe07a9082af5e
ansadmk <ansadmk@gmail.com>
2023-11-22 11:20:23 +0530

    new

--- mindmappr_js_frontend/src/app/redux/axiosInstance.jsx
+++ mindmappr_js_frontend/src/app/redux/axiosInstance.jsx
@@ -4,13 +4,13 @@ const cookie = getCookie("adminToken");
 const usercookie = getCookie("token");
 
  export const axiosInstanceAdmin=axios.create({
-    baseURL:"http://192.168.1.53:4000",
+    baseURL:"http://13.53.207.81:4000",
     headers: {
       Authorization: `Bearer ${cookie}`,
     },
   })
   export const axiosInstance=axios.create({
-    baseURL:"http://192.168.1.53:4000",
+    baseURL:"http://13.53.207.81:4000",
     headers: {
       Authorization: `Bearer ${usercookie}`,
     },
