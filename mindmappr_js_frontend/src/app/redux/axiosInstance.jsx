2ed0e9b8265e22c7613371aadf535e63877260a9
ansadmk <ansadmk@gmail.com>
2023-11-25 16:09:52 +0530

    error fix

--- mindmappr_js_frontend/src/app/redux/axiosInstance.jsx
+++ mindmappr_js_frontend/src/app/redux/axiosInstance.jsx
@@ -4,13 +4,14 @@ const cookie = getCookie("adminToken");
 const usercookie = getCookie("token");
 
  export const axiosInstanceAdmin=axios.create({
-    baseURL:"https://13.53.207.81",
+    // baseURL:"https://13.53.207.81",
+    baseURL:"https://mindmappr.onrender.com",
     headers: {
       Authorization: `Bearer ${cookie}`,
     },
   })
   export const axiosInstance=axios.create({
-    baseURL:"https://13.53.207.81",
+    baseURL:"https://mindmappr.onrender.com",
     headers: {
       Authorization: `Bearer ${usercookie}`,
     },
