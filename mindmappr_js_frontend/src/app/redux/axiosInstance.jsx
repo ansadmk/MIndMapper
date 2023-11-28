8e95685be482baa074d8b3a01aa6e089fb39067d
ansadmk <ansadmk@gmail.com>
2023-11-28 21:32:16 +0530

    final push

--- mindmappr_js_frontend/src/app/redux/axiosInstance.jsx
+++ mindmappr_js_frontend/src/app/redux/axiosInstance.jsx
@@ -5,13 +5,13 @@ const usercookie = getCookie("token");
 
  export const axiosInstanceAdmin=axios.create({
     // baseURL:"https://13.53.207.81",
-    baseURL:"https://mindmappr.onrender.com",
+    baseURL:"https://api.mindmappr.me/",
     headers: {
       Authorization: `Bearer ${cookie}`,
     },
   })
   export const axiosInstance=axios.create({
-    baseURL:"https://mindmappr.onrender.com",
+    baseURL:"https://api.mindmappr.me/",
     headers: {
       Authorization: `Bearer ${usercookie}`,
     },
